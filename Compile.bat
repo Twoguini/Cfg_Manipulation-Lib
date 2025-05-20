f55396186ea61ff28828455a68d272f63cff3cba
Guilherme Comunian <twoguini@gmail.com>
2025-05-20 16:03:36 -0300

    Building file structure

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,13 @@
-del testCFGFILE.dll, cfgFileTest_M.exe
+if not exist "./build" ( 
+  md build 
+  echo "Build File Created"
+) else (
+  cd ./build
+  del testCFGFILE.dll, cfgFileTest_M.exe
+  echo ".dll and .exe deleted"
+  cd ..\
+)
 
-gcc -shared -o testCFGFILE.dll cfgFile_M.c
+gcc -shared -o ./build/testCFGFILE.dll ./src/cfgFile_M.c
 
-gcc -o cfgFileTest_M.exe cfgFileTest_M.c -L. -ltestCFGFILE -I. -static-libgcc -static-libstdc++
+gcc -o ./build/cfgFileTest_M.exe ./src/cfgFileTest_M.c -L./build -ltestCFGFILE -I. -static-libgcc -static-libstdc++
